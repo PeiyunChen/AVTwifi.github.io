c50901f9e4b9593a9140b2e3e29ef6dea97e3a53
peiyunchen <peiyun1226@gmail.com>
2020-10-20 22:04:13 +0800

    202010202204

--- avt_wifi.js
+++ avt_wifi.js
@@ -4,20 +4,29 @@ function updateQRCode() {
    //console.log("hi");
    var pw = document.getElementById("password").value;
    var enc = document.getElementById("enc").value;
-   var hidden = document.getElementById("hidden").checked;
+   //var hidden = document.getElementById("hidden").checked;
    var qrcode = document.getElementById("qrcode");
    
    //console.log(pw + "\n");
    //console.log(enc + "\n");
    //console.log(hidden + "\n");
    //console.log(qrcode + "\n");
-   var text = "WIFI:S:" + ssid + ";T:" + enc + ";P:" + pw + ";H:" + hidden + ";;";
+   //var text = "WIFI:S:" + ssid + ";T:" + enc + ";P:" + pw + ";H:" + hidden + ";;";
    
    var text1 = "WIFI:S:Xiaomi_PC16u;T:WPA;P:shuhuilu;H:false;;";
+   var text2 = "WIFI:S:" + ssid + ";T:" + enc + ";P:" + pw + ";;";
    var date = new Date();
+   var a = localStorage.setItem("text",text2);
    //console.log(date.getDate());
-   qrcode.replaceChild(showQRCode(text), qrcode.lastChild);
+   qrcode.replaceChild(showQRCode(text2), qrcode.lastChild);
+
+}
+
+function changewindow(){
+   window.location.href = "wifi.html";
 }
 
 document.getElementById("form").onchange = updateQRCode;
+//var a  = localStorage.getItem("page");
+//console.log(a)
 //updateQRCode();
